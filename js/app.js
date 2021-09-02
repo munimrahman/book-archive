37aac62fd1e2222770f5bf81adb9b38363ceea5a
munimrahman <munimrh@gmail.com>
2021-09-02 08:54:20 +0600

    ui design completed

--- js/app.js
+++ js/app.js
@@ -1,33 +1,56 @@
 const searchField = document.getElementById('search-text');
-const searchButton = document.getElementById('search-button');
 const searchResultContainer = document.getElementById('search-result-contrainer');
+const errorMessage = document.getElementById('errorDiv');
+const spinner = document.getElementById("spinner");
 
-// searchButton.addEventListener('click', ()
 const loadData = () => {
+    // if (searchField.innerText === '') {
+    // errorDiv.innerText = "Search field cannot be empty.";
+    // }
+    errorMessage.style.display = 'none';
+
     const searchText = searchField.value;
-    searchField.value = '';
     searchResultContainer.innerHTML = '';
-    document.getElementById('total-items').innerText = '--';
+    document.getElementById('search-page-ui').style.display = 'none';
+    // document.getElementById('total-items').innerText = '--';
     const url = `https://openlibrary.org/search.json?q=${searchText}`;
+    spinner.classList.remove("d-none");
     fetch(url)
         .then(res => res.json())
-        .then(data => displayBooks(data))
+        .then(data => {
+            spinner.classList.add("d-none");
+            displayBooks(data)
+        })
+        .finally(() => {
+            searchField.value = '';
+        });
 };
 
 const displayBooks = (books) => {
-    document.getElementById('total-items').innerText = books.numFound;
-    // console.log(books);
-    books.docs.forEach(book => {
-        // console.log(book.title);
+    if (searchField.value === '') {
+        errorMessage.style.display = 'block';
+        errorMessage.style.color = 'red';
+        errorMessage.innerText = `Search Field Can't Be Empty.`;
+    }
+    else if (books.numFound === 0) {
+        errorMessage.style.display = 'block';
+        errorMessage.style.color = 'red';
+        errorMessage.innerText = `No Result Found For "${searchField.value}"`;
+    } else {
+        errorMessage.style.display = 'block';
+        errorMessage.style.color = 'green';
+        errorMessage.innerText = `${books.numFound} Items Found For "${searchField.value}"`;
+    }
+    books.docs.splice(0, 25).forEach(book => {
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
         <div class="card h-100">
-            <img src=" https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="">
+            <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" height="450px" class="card-img-top" alt="">
             <div class="card-body">
                 <p class="card-title"><span class="fw-bold">Book Name:</span> ${book.title}</p>
                 <p><span class="fw-bold">Book Author:</span> ${book.author_name}</p>
-                <p><span class="fw-bold">Publisher Name:</span> ${book.publisher}</p>
+                <p><span class="fw-bold">Publisher Name:</span> ${book.publisher.splice(0, 2)}</p>
                 <p><span class="fw-bold">First Published:</span> ${book.first_publish_year}</p>
             </div>
         </div>
